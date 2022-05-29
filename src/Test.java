1fc91823561998b7d819079830f28e9fe4380446
Sukhar <ssuxxarr@gmail.com>
2022-05-29 12:12:32 +0300

    add some answers

--- src/Test.java
+++ src/Test.java
@@ -1,18 +1,9 @@
 
 public class Test {
     public static void main(String[] args) {
-        StringBuilder s = new StringBuilder();
-        int x = 151;
-        while (x > 0) {
-            if (x % 2 == 1) {
-                s.append("1");
-            } else {
-                s.append("0");
-            }
-            x = x / 2;
-
-        }
-        s.reverse();
-        System.out.println(s);
+        short s= 256;
+        char c = '5';
+        int i = s + c;
+        System.out.println(i);
     }
 }
